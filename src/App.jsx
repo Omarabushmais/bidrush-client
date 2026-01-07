b6a0115e8bfd0984cbcec5d8e2cbe28231252201
Omar <Omarabushmais@gmail.com>
2026-01-07 15:36:51 +0300

    added auction details page and some tweaks to auction card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 import MainLayout from './Layout/MainLayout'
 import Home from './Pages/Home/Home'
 import Auctions from './Pages/Auctions/Auctions'
-import { Route, Routes } from 'react-router-dom'
 import About from './Pages/About/About'
 import Login from './Pages/Login/Login'
 import Register from './Pages/Register/Register'
+import AuctionDetails from './Pages/Auction Detail/AuctionDetails'
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Auctions' element={<Auctions/>} />
+        <Route path='Auctions/:id' element={<AuctionDetails/>}/>
         <Route path='/About' element={<About/>} />
         <Route path='/Login' element={<Login/>} />
         <Route path='/Register' element={<Register/>} />
--- src/Components/Auctions/Auc_card.jsx
+++ src/Components/Auctions/Auc_card.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from "./Auc_card.module.css"
+import { Link } from 'react-router-dom'
 
 function Auc_card({ auction }) {
   return (
@@ -9,7 +10,7 @@ function Auc_card({ auction }) {
       <h3 className={style.title}>{auction.title}</h3>
       <p className={style.bid}>Current Bid: ${auction.currentBid}</p>
       <p className={style.ends}>Ends in: {auction.endsIn}</p>
-      <button className={style.button}>View Auction</button>
+      <Link to={`/Auctions/${auction.id}`} className={style.button}>View Auction</Link>
 
     </div>
   )
