9e69fafd3e0ae855ebcb52e7879dafa968ae5276
Omar <Omarabushmais@gmail.com>
2026-01-22 15:19:20 +0300

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import MainLayout from './Layout/MainLayout'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './Pages/Home/Home'
 import Auctions from './Pages/Auctions/Auctions'
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import style from "./Footer.module.css"
-import logo from "../../assets/Logo.png"
+import React from 'react';
+import logo from "../../assets/Logo.png";
 
 function Footer() {
   return (
-    <footer className={style.footer}>
-      <img src={logo} alt="BidRush logo" className={style.footerLogo} />
+    <footer className="text-light py-4 text-center" style={{ backgroundColor: '#091B5A' }}>
+      <img src={logo} alt="BidRush logo" className="mb-2" style={{ height: '40px' }} />
 
-      <nav className={style.footerNav}>
-        <a href="/">Home</a>
-        <span>|</span>
-        <a href="/auctions">Auctions</a>
-        <span>|</span>
-        <a href="/about">About</a>
+      <nav className="d-flex justify-content-center gap-2 mb-2">
+        <a href="/" className="text-light text-decoration-none">Home</a>
+        <span className="text-light">|</span>
+        <a href="/auctions" className="text-light text-decoration-none">Auctions</a>
+        <span className="text-light">|</span>
+        <a href="/about" className="text-light text-decoration-none">About</a>
       </nav>
 
-      <p className={style.footerCopy}>
+      <p className="mb-0" style={{ fontSize: '0.875rem' }}>
         © 2025 BidRush. All rights reserved.
       </p>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
